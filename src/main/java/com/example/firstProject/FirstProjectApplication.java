f11af35ff750db0e53ba09d90b788d6726f44793
cube-manender <140604708+cube-manender@users.noreply.github.com>
2024-06-25 00:22:24 +0530

    changes

--- src/main/java/com/example/firstProject/FirstProjectApplication.java
+++ src/main/java/com/example/firstProject/FirstProjectApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.firstProject.config","com.example.firstProject.service"})
 //@ComponentScan(basePackages = {"com.example.firstProject", "com.example.firstProject.controllers", "com.example.firstProject.repositories"})
-
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
